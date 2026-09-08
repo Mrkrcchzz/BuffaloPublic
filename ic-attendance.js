db0f3c83081f7b4a4a046380713230d69a9532ac
Mrkrcchzz <mark@scholar-track.com>
2026-09-08 14:05:33 -0400

    Add self-invoking script to load attendance script

--- ic-attendance.js
+++ ic-attendance.js
@@ -1,1 +1,1 @@
-
+javascript:(function(){var s=document.createElement('script');s.src='https://cdn.jsdelivr.com/gh/Mrkrcchzz/BuffaloPublic@main/ic-attendance.js?v='+Date.now();document.body.appendChild(s);})();
